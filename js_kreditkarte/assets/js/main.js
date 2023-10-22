d2ebe86177a94b7d3144f5edb9bf9cf8e22f8af4
w1tch3r-code <rainerbrain@gmail.com>
2023-10-22 19:20:47 +0200

    JS Kreditkarte fertig gestellt

--- js_kreditkarte/assets/js/main.js
+++ js_kreditkarte/assets/js/main.js
@@ -6,3 +6,27 @@
 
 console.log("%c JS-Vertiefung – Kreditkarte", "color: tomato");
 
+const btnSubmit = document.querySelector("#btn-submit");
+
+btnSubmit.addEventListener("click", (event) => {
+	const input = document.querySelectorAll("input");
+	const info = document.querySelector(".info");
+	const cardNumberInput = document.querySelector("#card-number").value;
+	const cardHolderInput = document.querySelector("#card-holder").value;
+	const expirationDateInput = document.querySelector("#expiration-date").value;
+	const cardNumberOutput = document.querySelector(".card-number");
+	const cardHolderOutput = document.querySelector(".card-holder");
+	const expirationDateOutput = document.querySelector(".expiration-date");
+
+	for (let i = 0; i < input.length; i++) {
+		if (input[i].checkValidity() === false) {
+			info.textContent = "Bitte füllen Sie alle Felder korrekt aus!";
+            break;
+		} else {
+			cardNumberOutput.textContent = cardNumberInput;
+			cardHolderOutput.textContent = cardHolderInput;
+			expirationDateOutput.textContent = expirationDateInput;
+		}
+        info.textContent = "Alle Felder wurden korrekt ausgefüllt!";
+	}
+});
